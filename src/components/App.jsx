f359f0be9f1adec5b5b659c66da38b3f51617588
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-03-15 18:51:26 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,127 @@
-export const App = () => {
+import React, { useState, useEffect } from 'react';
+import Section from './Section/Section';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Statistics from './Statistics/Statistics';
+import Notification from './Notification/Notification';
+
+export default function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [positivePers, setPositivePers] = useState(0);
+
+  const handleBtnClick = label => {
+    // console.log(label)
+
+    switch (label) {
+      case 'good':
+        setGood(prevStateGood => prevStateGood + 1);
+        break;
+
+      case 'neutral':
+        setNeutral(prevStateNeutral => prevStateNeutral + 1);
+        break;
+
+      case 'bad':
+        setBad(prevStateBad => prevStateBad + 1);
+        break;
+
+      default:
+        console.log('any results');
+    }
+  };
+
+  useEffect(() => {
+    const total = good + neutral + bad;
+    setTotal(total);
+  }, [good, neutral, bad]);
+
+  useEffect(() => {
+    setPositivePers(((good / total) * 100).toFixed(0));
+  }, [total]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          labels={['good', 'neutral', 'bad']}
+          onLeaveFeedback={handleBtnClick}
+        />
+      </Section>
+      <Section title="Statistics">
+        {total > 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePers={positivePers}
+          />
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section>
+    </>
   );
-};
+}
+
+// class OldApp extends Component {
+//   state = {
+//     good: 0,
+//     neutral: 0,
+//     bad: 0,
+//   };
+
+//   handleBtnClick = label => {
+//     this.setState(prevState => {
+//       return { [label]: prevState[label] + 1 };
+//     });
+//   };
+
+//   countTotal = () => {
+//     const totalFBs = this.state.good + this.state.neutral + this.state.bad;
+//     return totalFBs;
+//   };
+
+//   countPercentage = () => {
+//     const totalFBs = (
+//       (this.state.good /
+//         (this.state.good + this.state.neutral + this.state.bad)) *
+//       100
+//     ).toFixed(0);
+//     return totalFBs;
+//   };
+
+//   render() {
+//     const { good, neutral, bad } = this.state;
+//     const total = this.countTotal();
+//     const positivePers = this.countPercentage();
+
+//     return (
+//       <>
+//         <Section title="Please leave feedback">
+//           <FeedbackOptions
+//             labels={['good', 'neutral', 'bad']}
+//             onLeaveFeedback={this.handleBtnClick}
+//           />
+//         </Section>
+//         <Section title="Statistics">
+//           {total > 0 ? (
+//             <Statistics
+//               good={good}
+//               neutral={neutral}
+//               bad={bad}
+//               total={total}
+//               positivePers={positivePers}
+//             />
+//           ) : (
+//             <Notification message="There is no feedback" />
+//           )}
+//         </Section>
+//       </>
+//     );
+//   }
+// }
+
+// export default OldApp;
